4c18aef5f8267c9bfced1f3b80b6165f6efaf678
rtyty0615 <174629541+rtyty0615@users.noreply.github.com>
2026-01-19 15:00:42 +0800

    Fix issues with 0 and decimal

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 let sum = 0;
-let a = "";
+let a = 0;
 let b = "";
 let c = "";
 
@@ -8,44 +8,83 @@ const allBtns = document.querySelectorAll("button");
 allBtns.forEach((btn) => {
     btn.addEventListener("click", () => {
         if (btn.id === "clear"){
-            a = b = c = "";
-            sum = 0;
+            a = sum = 0;
+            b = c = "";
             console.log(a, b, c, sum);
-        } else if (a === ""){
-            if (btn.classList.contains("number")) {
-                a = btn.textContent;
-                console.log(a, b, c);}
         } else if (b === "") {
-            if (btn.classList.contains("number")) {
-                a+= btn.textContent;
-                console.log(a, b, c);
+            if (a === 0) {
+                if (btn.classList.contains("number")) {
+                    if (btn.id !== "zero") {
+                        if (btn.id === "decimal") {
+                            a += btn.textContent;
+                            console.log(a, b, c);
+                        }
+                        else {
+                            a = btn.textContent;
+                            console.log(a, b, c);
+                        }
+                    }
+                } else if (btn.classList.contains("operator")) {
+                    b = btn.id;
+                    console.log(a, b, c);
+                }
+            } else if (btn.classList.contains("number")) {
+                if (btn.id === "decimal" && String(a).includes(".")) {
+                    console.log(a, b, c);
+                } 
+                // fixed zero
+                else {
+                    a+= btn.textContent;
+                    console.log(a, b, c);
+                }
             } else if (btn.classList.contains("operator")) {
                 b = btn.id;
                 console.log(a, b, c);
             } 
         } else if (c === "") {
             if (btn.classList.contains("number")) {
-                c = btn.textContent;
-                console.log(a, b, c);
+                if (btn.id === "decimal"){
+                    c = 0;
+                    c += btn.textContent;
+                    console.log(a, b, c);
+                } else {
+                    c = btn.textContent;
+                    console.log(a, b, c);
+                }
             } else if (btn.classList.contains("operator")) {
                 b = btn.id;
                 console.log(a, b, c);
             } 
-        } else if (a !== "" && b !== "" && c !== "" ) {
+        } else if ( b !== "" && c !== "" ) {
             if (btn.classList.contains("number")) {
-                c += btn.textContent;
-                console.log(a, b, c);
-                console.log(a, b, c, sum);
+                if (btn.id === "decimal" && String(a).includes(".")){
+                    console.log(a, b, c, sum);
+                } else if ( c === "0") {
+                    c = "";
+                    c += btn.textContent;
+                    console.log(a, b, c, sum);
+                } else {
+                    c += btn.textContent;
+                    console.log(a, b, c, sum);
+                }
             } else if (btn.id === "equals") {
-                operate(a,b,c)
-                console.log(a, b, c, sum);
+                if (b === "divide" && c === "0"){
+                    alert("You can't divide by 0!");
+                } else{
+                    operate(a,b,c);
+                    console.log(a, b, c, sum);
+                }
             } else if (btn.classList.contains("operator")){
+                if (b === "divide" && c === "0") {
+                    alert("You can't divide by 0!");
+                } else {
                 operate(a, b, c);
                 console.log(a, b, c, sum);
                 b = btn.id;
                 a = sum;
                 c = "";
                 console.log(a, b, c, sum);
+                }
             }
         }
     });
@@ -74,3 +113,5 @@ function operate(a,b,c){
     }
 
 }
+
+
