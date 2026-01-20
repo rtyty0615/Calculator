b671c0e8452abbcaacb1fb11ddce390361e30591
rtyty0615 <174629541+rtyty0615@users.noreply.github.com>
2026-01-20 15:29:23 +0800

    Add keyboard support

--- script.js
+++ script.js
@@ -162,5 +162,62 @@ function operate(a,b,c){
     }
 
 }
-
-
+// add keyboard
+document.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case "1":
+            document.getElementById('one').click();
+            break;
+        case "2":
+            document.getElementById('two').click();
+            break;
+        case "3":
+            document.getElementById('three').click();
+            break;
+        case "4":
+            document.getElementById('four').click();
+            break;
+        case "5":
+            document.getElementById('five').click();
+            break;
+        case "6":
+            document.getElementById('six').click();
+            break;
+        case "7":
+            document.getElementById('seven').click();
+            break;
+        case "8":
+            document.getElementById('eight').click();
+            break;
+        case "9":
+            document.getElementById('nine').click();
+            break; 
+        case "0":
+            document.getElementById('zero').click();
+            break;
+        case "/":
+            document.getElementById('divide').click();
+            break;
+        case "*":
+            document.getElementById('multiply').click();
+            break;
+        case "-":
+            document.getElementById('subtract').click();
+            break;
+        case "+":
+            document.getElementById('add').click();
+            break;
+        case "Enter":
+            document.getElementById('equals').click();
+            break;
+        case "Escape":
+            document.getElementById('clear').click();
+            break;
+        case "Backspace":
+            document.getElementById('delete').click();
+            break;
+        case ".":
+            document.getElementById('decimal').click();
+            break;           
+    }
+});
